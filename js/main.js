1bc7aaa38b232825591715c942471ff238ac6477
bender <123@gmail.com>
2017-12-01 00:40:04 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,213 @@
+var account;
+var accountHash;
+
+$(document).ready(function(){
+    var accountParam = getQueryParams(document.location.search).account;
+    document.getElementById('accountInput').placeholder = accountParam ? "Current: " + accountParam : "Account";
+    account = accountParam ? accountParam : 0;
+    // $("#tabs").tabs();
+    $('#workersTable').DataTable({
+        info: false,
+/*
+        responsive: {
+            details: {
+                display: $.fn.dataTable.Responsive.display.modal( {
+                    header: function ( row ) {
+                        return 'Details:';
+                    }
+                } ),
+                renderer: function ( api, rowIdx, columns ) {
+                    var data = $.map( columns, function ( col, i ) {
+                        return '<tr>'+
+                            '<td>'+col.title+':'+'</td> '+
+                            '<td>'+col.data+'</td>'+
+                            '</tr><tr><td></td><td></td></tr><tr><td></td><td></td></tr>';
+                    } ).join('');
+
+                    return $('<table/>').append( data );
+                }
+            }
+        }
+*/
+    });
+    $('#calculatorTable').DataTable({
+        info: false,
+        paging: false,
+        searching: false,
+/*
+        responsive: {
+            details: {
+                display: $.fn.dataTable.Responsive.display.modal( {
+                    header: function ( row ) {
+                        return 'Details:';
+                    }
+                } ),
+                renderer: function ( api, rowIdx, columns ) {
+                    var data = $.map( columns, function ( col, i ) {
+                        return '<tr>'+
+                            '<td>'+col.title+':'+'</td> '+
+                            '<td>'+col.data+'</td>'+
+                            '</tr><tr><td></td><td></td></tr><tr><td></td><td></td></tr>';
+                    } ).join('');
+
+                    return $('<table/>').append( data );
+                }
+            }
+        }
+*/
+    });
+    // updateHashRate();
+    updateTabs();
+});
+
+setInterval(function() {
+    "use strict";
+    // updateHashRate();
+    updateTabs();
+}, 60000);
+
+function updateHashRate() {
+    "use strict";
+    getAndUpdateRate("hashrate");
+    getAndUpdateRate("average_hashrate");
+    getAndUpdateRate("last_reported_hashrate");
+}
+
+
+function getAndUpdateRate(hashType) {
+    var url, data;
+    switch (hashType) {
+        case ("hashrate") :
+            // url = "http://monopool.io/api/eth/hashrate";
+            url = "https://api.nanopool.org/v1/eth/hashrate/" + account;
+            data = JSON.stringify([account]);
+            break;
+        case ("average_hashrate") :
+            // url = "http://monopool.io/api/eth/avghashratelimited";
+            url = "https://api.nanopool.org/v1/eth/avghashratelimited/" + account + "/6";
+            data = JSON.stringify([account, "6"]);
+            break;
+        case ("last_reported_hashrate") :
+            // url = "http://monopool.io/api/eth/reportedhashrate";
+            url = "https://api.nanopool.org/v1/eth/reportedhashrate/" + account;
+            data = JSON.stringify([account]);
+            break;
+    }
+    $.ajax({
+        // type:"POST",
+        type:"GET",
+        url: url,
+        dataType: "json",
+        // data: data,
+        crossDomain : true
+    }).done(function (responseData) {
+        updateHashRateData(hashType ,responseData);
+    }).fail(function (xhr, status, error) {
+        console.log("error:");
+        console.log(status);
+        console.log(xhr.responseText);
+    });
+}
+
+function updateHashRateData(type, data){
+    "use strict";
+    var bind;
+    switch (type) {
+        case "hashrate":
+            bind = document.querySelector('div[data-bind="hashrate"]');
+            accountHash = data.data;
+            break;
+        case "average_hashrate":
+            bind = document.querySelector('div[data-bind="average_hashrate"]');
+            break;
+        case "last_reported_hashrate":
+            bind = document.querySelector('div[data-bind="last_reported_hashrate"]');
+            break;
+    }
+    bind.innerHTML = roundTo(data.data, 2) + " ,Mh/s";
+}
+
+function updateTabs(){
+    "use strict";
+    getAndUpdateTabs("workers");
+    getAndUpdateTabs("calculator");
+}
+
+function getAndUpdateTabs(tab) {
+    var url, data;
+    switch (tab) {
+        case ("workers") :
+            // url = "http://monopool.io/api/eth/workers";
+            url = "https://api.nanopool.org/v1/eth/workers/" + account;
+            data = JSON.stringify([account]);
+            break;
+        case ("calculator") :
+            // url = "http://monopool.io/api/eth/approximated_earnings";
+            if (!accountHash) {
+                setTimeout(function(){
+                    getAndUpdateTabs("calculator");
+                }, 100);
+                return;
+            }
+            url = "https://api.nanopool.org/v1/eth/approximated_earnings/" + accountHash;
+            data = JSON.stringify([accountHash]);
+            break;
+    }
+    $.ajax({
+        // type:"POST",
+        type:"GET",
+        url: url,
+        dataType: "json",
+        // data: data,
+        crossDomain : true
+    }).done(function (response) {
+        updateTabData(tab, response.data);
+    }).fail(function (xhr, status, error) {
+        console.log("error:");
+        console.log(status);
+        console.log(xhr.responseText);
+    });
+}
+
+function updateTabData(tab, responseData) {
+    switch (tab) {
+        case "workers":
+            var workersTable = $('#workersTable').DataTable();
+            workersTable.clear();
+            workersTable.rows.add(convertWorkersData(responseData));
+            workersTable.draw();
+            break;
+        case "calculator":
+            var calculatorTable = $('#calculatorTable').DataTable();
+            calculatorTable.clear();
+            calculatorTable.rows.add(convertCalculatorData(responseData));
+            calculatorTable.draw();
+            break;
+    }
+}
+
+function convertWorkersData(responseData) {
+    "use strict";
+    if (!responseData) {
+        return [];
+    }
+    var convertedData = [];
+    for (var i = 0; i < responseData.length; i++) {
+        var data = responseData[i];
+        convertedData.push([data.id, convertToDateCalculated(data.lastShare * 1000), data.rating, data.hashrate]);
+    }
+    return convertedData;
+}
+
+function convertCalculatorData(responseData) {
+    "use strict";
+    var convertedData = [];
+
+    for (var property in responseData) {
+        if (responseData.hasOwnProperty(property)) {
+            var dataObject = responseData[property];
+            convertedData.push([property, dataObject.coins, dataObject.bitcoins, roundTo(dataObject.dollars, 2)]);
+        }
+    }
+    return convertedData;
+}
---
+++ js/plugins.js
@@ -0,0 +1,81 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+// Place any jQuery/helper plugins in here.
+
+function roundTo(n, digits) {
+    var negative = false;
+    if (digits === undefined) {
+        digits = 0;
+    }
+    if( n < 0) {
+        negative = true;
+        n = n * -1;
+    }
+    var multiplicator = Math.pow(10, digits);
+    n = parseFloat((n * multiplicator).toFixed(11));
+    n = (Math.round(n) / multiplicator).toFixed(2);
+    if( negative ) {
+        n = (n * -1).toFixed(2);
+    }
+    return n;
+}
+
+function getQueryParams(qs) {
+    qs = qs.split('+').join(' ');
+
+    var params = {},
+        tokens,
+        re = /[?&]?([^=]+)=([^&]*)/g;
+
+    while (tokens = re.exec(qs)) {
+        params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+    }
+
+    return params;
+}
+
+function convertToDateCalculated(millis) {
+    /**
+     * Нормализация значений дат под европейский формат
+     * @param data
+     * @returns {string}
+     */
+    function normalizeDateValues(data) {
+        return data < 10 ? "0" + data : data;
+    }
+
+    var dateReadable = new Date(millis);
+    var year = dateReadable.getFullYear();
+    var month = normalizeDateValues(dateReadable.getMonth() + 1);
+    var day = normalizeDateValues(dateReadable.getDate());
+    var hour = normalizeDateValues(dateReadable.getHours());
+    var minutes = normalizeDateValues(dateReadable.getMinutes());
+    var seconds = normalizeDateValues(dateReadable.getSeconds());
+
+    return day + "." + month + "." + year + " " + hour + ":" + minutes + ":" + seconds;
+}
+
+function goToAccountPage(){
+    window.location.href = "index.html?account=" + document.getElementById('accountInput').value;
+}
